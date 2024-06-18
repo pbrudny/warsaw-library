74872fd579b532abfd652f8f40998ee4ce9805bf
Piotr Brudny <pbrudny@gmail.com>
2024-06-18 14:44:22 +0200

    Add Building

--- library.py
+++ library.py
@@ -1,4 +1,6 @@
-class Library:
+from building import Building
+
+class Library(Building):
     def __init__(self, location, name):
         self.location = location
         self.name = name
---
+++ main.py
@@ -0,0 +1,4 @@
+from library import Library
+
+my_library = Library('Sulkiewicza 1, Warsaw', 'Main building')
+print(my_library.name)
